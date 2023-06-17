8433fe68c9cc5dc128c0d5d1bfdd4b4eb708beab
nevesfelipe47 <santosfelpe47@gmail.com>
2023-06-17 20:18:16 -0300

    Comentario da Aula Aprendendo Python

---
+++ curso_python/aula4.py
@@ -0,0 +1,15 @@
+# Tipod int e float
+# int -> Número inteiro
+# O tipo int representa qualquer número
+# positivo ou negativo. int sem sinal é 
+# positivo.
+
+
+#Float -> Número com ponto flutuante 
+#O tipo float representa qualquer número
+# positivo ou negativo com ponto flutuante. 
+# float sem sinal é considerado positivo 
+
+
+# A função type mostra o tipo que o Python 
+# inferiu ao valor.
