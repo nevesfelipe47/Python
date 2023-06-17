43d3db2bc024b909c1df9acb6a7f91db0357f1ab
nevesfelipe47 <santosfelpe47@gmail.com>
2023-06-17 20:12:25 -0300

    QUARTO commit Aprendendo Python

--- curso_python/aula3.py
+++ curso_python/aula3.py
@@ -1,4 +1,6 @@
 """
+
+DocString
 Python = Linguagem de programação
 Tipo de tipagem = Dinamica / Forte
 
@@ -8,9 +10,11 @@ Strings são textos que estão dentro de aspas
 """
 
 # Aspas Simples 
+print('Felipe Neves')
 
 # Aspas duplas 
-
+print("Felipe Neves")
 # Escape
-
+print("Felipe Neves \" Jéssica paloma ") # Não aparece a barra 
 # r
+print(r"Felipe Neves \" Jéssica paloma ") # Vai aparecer a barra 
