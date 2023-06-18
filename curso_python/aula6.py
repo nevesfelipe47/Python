dacc60988a045701e30d33fa8ed3857599ee5e76
nevesfelipe47 <santosfelpe47@gmail.com>
2023-06-18 07:51:51 -0300

    SEXTO commit Aprendendo Python

---
+++ curso_python/aula6.py
@@ -0,0 +1,9 @@
+""" 
+    Conversão de tipos, coerção
+    type convertion, typecasting, coercion
+    é o ato de converter um tipo em outro
+    tipos imutavei e primitivos:
+    str, int, float, bool
+"""
+
+print('1'+1)
