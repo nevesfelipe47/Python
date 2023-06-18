a8a8a58fff37b1280c37da7c2b653073044f44eb
nevesfelipe47 <santosfelpe47@gmail.com>
2023-06-18 06:55:46 -0300

    QUINTO commit Aprendendo Python

---
+++ curso_python/aula5.py
@@ -0,0 +1,18 @@
+"""
+    Tipo de dado bool (boolean)
+    Ao questionar algo em um programa,
+    só existem duas respostas possiveis: 
+    sim (True) ou não (false).
+    Existem varios operadores para "questionar".
+    Dentre eles o ==, que é um operador lógico que 
+    questiona se um valor é igual a outro
+
+"""
+
+print(10==10) # Sim => True (Verdadeiro)
+print(1==-1)  # Não => False (Falso)
+
+print (type(True))
+print (type(False))
+print(type(10==10)) # Retornar o tipo 
+print(type(1==-1))  # Retornar o tipo 
