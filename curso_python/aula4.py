2700410e88ecafbba85fcaa514b5d0eb33f2fcae
nevesfelipe47 <santosfelpe47@gmail.com>
2023-06-18 06:09:59 -0300

    QUARTO commit Aprendendo Python

--- curso_python/aula4.py
+++ curso_python/aula4.py
@@ -4,12 +4,21 @@
 # positivo ou negativo. int sem sinal é 
 # positivo.
 
+print(11,'Esses são do tipo int' ,sep=' -> ')
+print(-11, 'Esses são do tipo int' ,sep=' -> ')
+print(0 ,'Esses são do tipo int' ,sep=' -> ')
+
 
 #Float -> Número com ponto flutuante 
 #O tipo float representa qualquer número
 # positivo ou negativo com ponto flutuante. 
 # float sem sinal é considerado positivo 
 
+print(1.1,0.1, 'Esses são do tipo float' ,sep=' -> ')
 
 # A função type mostra o tipo que o Python 
 # inferiu ao valor.
+
+print(type('Felipe'))
+print(type(1.1111111))
+print(type(11.11),type('Jessica'),type(24))
