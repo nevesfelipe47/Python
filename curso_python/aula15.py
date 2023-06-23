a5aa5de0b34a49675c8911ea945ba58cfd648b14
nevesfelipe47 <santosfelpe47@gmail.com>
2023-06-23 18:26:28 -0300

    parei de contar os  commit Aprendendo Python

---
+++ curso_python/aula15.py
@@ -0,0 +1,7 @@
+nome = input('Qual teu nome bença? ')
+
+idade = int(input('Tua idade comparsa? '))
+
+
+print(type(idade), type(nome))
+
---
+++ curso_python/aula16.py
@@ -0,0 +1,8 @@
+entrada = input("Você quer entrar ou sair ")
+
+if entrada == 'entrar':
+        print("Você entrou no sistema ")
+elif entrada != 'entrar' and entrada !='sair':
+        print("Opção invalida!")
+else:
+    print("Saiu do programa")
